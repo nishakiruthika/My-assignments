4f0fb1116570f2a9495b9419aa07e4d69ddf05d7
nishakiruthika <71579732+nishakiruthika@users.noreply.github.com>
2025-01-19 18:04:56 +0530

    Add files via upload
    HomeAssignments_Day1

---
+++ Homeassignments/Datatypesandvariables.java
@@ -0,0 +1,25 @@
+package Homeassignments;
+
+public class Datatypesandvariables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		float version=90.1f;
+		String value = "google";
+		boolean IsBeta =false;
+		int releaseyear =2008;
+		char shortcutKey ='C';
+		
+		System.out.println("Version :" +version);
+		System.out.println("String Value :" +value);
+		System.out.println("IsBeta :" +IsBeta);
+		System.out.println("ReleaseYear :" +releaseyear);
+		System.out.println("ShortcutKey:" +shortcutKey);
+		
+		
+
+	}
+
+}
